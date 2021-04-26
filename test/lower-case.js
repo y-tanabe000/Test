bf4844bb8332f4ec7ff799b713a8f04b2b270346
yuka <tanabe-y@ohs-ndd.co.jp>
2021-04-26 10:30:13 +0900

    てすと

--- test/lower-case.js
+++ test/lower-case.js
@@ -1,7 +1,7 @@
 module.exports = function(RED) {
-    //関数定義
+    
     function LowerCaseNode(config) {
-        //全てのノードで共有する機能を初期化
+        
         RED.nodes.createNode(this,config);
         var node = this;
         node.on('input', function(msg) {
@@ -9,6 +9,6 @@ module.exports = function(RED) {
             node.send(msg);
         });
     }
-    //ノード名を指定し、LowerCaseNodeノードをランタイムへ登録
+    
     RED.nodes.registerType("lower-case",LowerCaseNode);
 }
