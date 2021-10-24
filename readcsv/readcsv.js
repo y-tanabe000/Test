ad8751989f7ffd877fa493e6c6d85becac6e90a1
yuka <tanabe-y@ohs-ndd.co.jp>
2021-10-24 17:20:21 +0900

    testcsv

---
+++ readcsv/readcsv.js
@@ -0,0 +1,15 @@
+module.exports = function(RED) {
+    
+    function readcsv(config) {
+        
+        RED.nodes.createNode(this,config);
+        var node = this;
+        const fs = require('fs');
+        const csv = require('csv');
+
+        fs.createReadStream(__dirname + 'test.csv')
+        .pipe(process.stdout);
+    }
+    
+    RED.nodes.registerType("readcsv",readcsv);
+}
